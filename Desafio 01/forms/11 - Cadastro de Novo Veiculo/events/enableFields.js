bdac77691722bb65b738028a031acc384f9f3ab6
Rogério Carvalho <rogerio.dev@outlook.com>
2023-08-21 20:28:30 -0300

    Fix

--- Desafio 01/forms/11 - Cadastro de Novo Veiculo/events/enableFields.js
+++ Desafio 01/forms/11 - Cadastro de Novo Veiculo/events/enableFields.js
@@ -1,1 +1,18 @@
-function enableFields(form){ }
+function enableFields(form){
+    var atividadeInicio      = 4;
+    var aprovDiretoria       = 5;
+    var inclusaoPratrimonio  = 15;
+
+    var CURRENT_STATE = getValue("WKNumState");
+
+    if(CURRENT_STATE != atividadeInicio){
+        form.setEnabled('marcaVeiculo', false);
+        form.setEnabled('corVeiculo', false);
+        form.setEnabled('anoVeiculo', false);
+        form.setEnabled('valorVeiculo', false);
+        form.setEnabled('dataCompraVeiculo', false);
+        form.setEnabled('zoom_estados', false);
+        form.setEnabled('chassiVeiculo', false);
+        form.setEnabled('renavanVeiculo', false);       
+    };
+ }
